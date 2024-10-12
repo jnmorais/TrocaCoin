f3ddc615bf5cb280faf0c5e06f8bdf0def1afcb9
jnmorais <jsmorais2002@gmail.com>
2024-10-12 00:15:36 -0300

    Criando a pagina de listagem, pasta service e integrando com a api

--- TrocaCoin/src/app/app-routing.module.ts
+++ TrocaCoin/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ListagemMoedasComponent } from './pages/listagem-moedas/listagem-moedas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PrincipalComponent},
+  {path: 'listagem-moedas',component: ListagemMoedasComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TrocaCoin/src/app/app.module.ts
+++ TrocaCoin/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './principal/principal.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ListagemMoedasComponent } from './pages/listagem-moedas/listagem-moedas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    ListagemMoedasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
